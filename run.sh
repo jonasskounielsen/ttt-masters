8d3852abc05c8a4ce75b53245e3eaaca7e89574e
jonasskounielsen <jonasskou.n@gmail.com>
2025-08-26 12:58:54 +0200

    Update binaries and gitignore

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 set -e
 rustc ./src/lib.rs -o ./rustbot.so
 g++ ./interface/interface.cpp -fPIC -shared -o ./"AI's"/bot/libbot.so
+unlink ./CROSS
+unlink ./DOT
+ln -sf ./"AI's"/hand ./CROSS
+ln -sf ./"AI's"/bot ./DOT
 steam-run ./TTT-masters
